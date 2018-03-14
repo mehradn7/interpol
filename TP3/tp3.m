593f8a361132d4a70fc4e49aeb657bd9a1245613
mehrad rafii <mehrad.rafii@yahoo.fr>
2018-03-14 16:09:27 +0100

    partie 1 du projet terminée

--- TP3/tp3.m
+++ TP3/tp3.m
@@ -2,14 +2,14 @@
 [X,Y] = saisi_points();
 
 %% Courbes fermées
-degre = 3; % degré des splines
+degre = 7; % degré des splines
 
 [X_spline, Y_spline] = subdivise_ferme(1, degre, X,Y);
 plot([X,X(1)],[Y,Y(1)]);
 plot(X_spline, Y_spline, 'x-');
 
 %% Courbes ouvertes
-[X_spline, Y_spline] = subdivise_ouvert(1, degre, X,Y);
+% [X_spline, Y_spline] = subdivise_ouvert(1, degre, X,Y);
 
 plot(X_spline, Y_spline);
 
---
+++ projet/applique_subdivision.m
@@ -0,0 +1,16 @@
+function [ M_res ] = applique_subdivision( M, nb_iter, degre )
+% Prend en paramètre une matrice M représentant les abscisses d'une grille
+% de points
+% Renvoie une matrice M_res résultat de l'application de subdivise_ferme
+% sur chaque ligne de la matrice M
+
+quelconque = zeros(1,42);
+
+M_res = zeros(size(M,1), (2^nb_iter)*size(M,2) + 1);
+for i=1:size(M,1)
+    [M_i_res, ~] = subdivise_ferme(nb_iter,degre,M(i,:), quelconque);
+    M_res(i,:) = M_i_res;
+end
+
+end
+
